150c13f1af275653585f81e41186768cf524ea69
João <jlcarruda3@gmail.com>
2023-10-16 01:47:23 -0300

    feat: added shell flags for cook-rice.sh and inject-nixos-config.sh

--- cook-rice.sh
+++ cook-rice.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 
+print_help () {
+  echo "Install and copy configuration NixOS Ricing"
+  echo "Usage: $0 [-h] [-o <only>]"
+  echo "  -h: Print this help"
+  echo "  -o: Install only the specified package (hardware, os, cachix, all)"
+}
+
+while getopts "ho:" opt; do
+  case "$opt" in
+    h) print_help; exit 0 ;;
+    o) only=$OPTARG ;;
+  esac
+done
+
+
 # Install Home-manager
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
 sudo nix-channel --update
 
+if [ -z "$only" ]; then
+  sudo ./inject-nixos-config.sh
+else
+  sudo ./inject-nixos-config.sh -o $only
+fi
+
 # Inject nixos config
-sudo ./inject-nixos-config.sh
 
 # setup config
 ./setup-config.sh
--- inject-nixos-config.sh
+++ inject-nixos-config.sh
@@ -1,25 +1,65 @@
 #!/bin/sh
 
-if [[ $EUID > 0 ]]; then
-    echo "Please run as SUDO for injecting new nixos config. Aborting..."
-    exit 1
+while getopts "ho:" opt; do
+  case "$opt" in
+  h)
+    print_help
+    exit 0
+    ;;
+  o) only=$OPTARG ;;
+  esac
+done
+
+if [ -z "$only" ]; then
+  $only='all'
 fi
 
-nixos_dir="/etc/nixos" # CHANGE IT TO /etc/nixos when running
+if [[ $EUID ]] >0; then
+  echo "Please run as SUDO for injecting new nixos config. Aborting..."
+  exit 1
+fi
+
+nixos_dir="/etc/nixos"
 
 if [ -d "$nixos_dir/.backup" ]; then
-    rm -rf "$nixos_dir/.backup"
+  rm -rf "$nixos_dir/.backup"
 fi
 
 mkdir -p "$nixos_dir/.backup"
-# backing up
-cp "$nixos_dir/cachix.nix" "$nixos_dir/.backup"
-cp -r "$nixos_dir/cachix" "$nixos_dir/.backup"
-cp "$nixos_dir/configuration.nix" "$nixos_dir/.backup"
-cp "$nixos_dir/hardware-configuration.nix" "$nixos_dir/.backup"
 
-# inject
-cp -r ./nixos/* "$nixos_dir"
-# cp -r ./nixos/configuration.nix "$nixos_dir"
+backup() {
+  cp "$nixos_dir/cachix.nix" "$nixos_dir/.backup"
+  cp -r "$nixos_dir/cachix" "$nixos_dir/.backup"
+  cp "$nixos_dir/configuration.nix" "$nixos_dir/.backup"
+  cp "$nixos_dir/hardware-configuration.nix" "$nixos_dir/.backup"
+}
+
+inject() {
+  backup
+  if [ "$only" = "all" ]; then
+    _inject_cachix
+    _inject_config_os
+    _inject_hardware
+  elif [ "$only" = "cachix" ]; then
+    _inject_cachix
+  elif [ "$only" = "os" ]; then
+    _inject_config_os
+  elif [ "$only" = "hardware" ]; then
+    _inject_hardware
+  fi
+}
+
+_inject_cachix() {
+  cp -r ./nixos/cachix "$nixos_dir"
+  cp -r ./nixos/cachix.nix "$nixos_dir"
+}
+
+_inject_config_os() {
+  cp -r ./nixos/configuration.nix "$nixos_dir"
+}
+
+_inject_hardware() {
+  cp -r ./nixos/hardware-configuration.nix "$nixos_dir"
+}
 
 chown -R root:root "$nixos_dir"
